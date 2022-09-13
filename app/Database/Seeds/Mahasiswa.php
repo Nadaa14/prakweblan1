b6c2f229595c1daa3b48ade665d5add9e264baab
anzaniy <78955667+Nadaa14@users.noreply.github.com>
2022-09-14 00:53:04 +0700

    au ah

---
+++ app/Database/Seeds/Mahasiswa.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I186\Time;
+
+class Mahasiswa extends Seeder
+{
+    public function run()
+    {
+        $data_mahasiswa = [
+            [
+                'npm' => '2017051057',
+            'nama'    => 'Nadaa Azhar',
+            'alamat' => 'Bandarlampung',
+            'created_at' => Time::now()
+        ],
+
+            [
+                'npm' => '2017051057',
+            'nama'    => 'Nadaa Azhar',
+            'alamat' => 'Bandarlampung',
+            'created_at' => Time::now()
+        ],
+
+            [
+                'npm' => '2017051057',
+            'nama'    => 'Nadaa Azhar',
+            'alamat' => 'Bandarlampung',
+            'created_at' => Time::now()
+        ],
+        ];
+
+        // Simple Queries
+       // $this->db->query('INSERT INTO mahasiswa (npm, nama, alamat, created_at) VALUES(:npm:, :nama:, :alamat:, :created_at:)', $data);
+
+        // Using Query Builder
+        foreach($data_mahasiswa as $data){
+            $this->db->table('mahasiswa')->insert($data);
+        }
+    }
+}
